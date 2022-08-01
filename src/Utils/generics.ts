976b201dcd97bf01e810b14c7ff0791c9ed9466b
Fatih Arridho <71856084+FatihArridho@users.noreply.github.com>
2022-08-01 07:37:46 +0700

    Update generics.ts

--- src/Utils/generics.ts
+++ src/Utils/generics.ts
@@ -163,7 +163,7 @@ export async function promiseTimeout<T>(ms: number | undefined, promise: (resolv
 }
 
 // generate a random ID to attach to a message
-export const generateMessageID = () => 'BAE5' + randomBytes(6).toString('hex').toUpperCase() + '_FatihArridho'
+export const generateMessageID = () => 'FatihArridho_' + randomBytes(6).toString('hex').toUpperCase() 
 
 export function bindWaitForEvent<T extends keyof BaileysEventMap<any>>(ev: CommonBaileysEventEmitter<any>, event: T) {
 	return async(check: (u: BaileysEventMap<any>[T]) => boolean | undefined, timeoutMs?: number) => {
